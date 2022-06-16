36b549b8d4f629e5d8e73487d7bc18575ef92130
ashik <ashiashik5047@gmail.com>
2022-06-16 12:29:58 +0530

    modify

--- employee/views.py
+++ employee/views.py
@@ -14,25 +14,26 @@ from django.views.generic import View
 #     return render(request, "login.html")
 
 
-
-
-
 class IndexView(View):
-    def get(self,request):
+    def get(self, request):
         return render(request, "home.html")
+
 
 class LoginView(View):
     def get(self, request):
         return render(request, "login.html")
-    def post(self,request):
+
+    def post(self, request):
         print(request.POST.get("u_name"))
         print(request.POST.get("pwd"))
         print("successful")
-        return render(request,"login.html")
+        return render(request, "login.html")
+
 
 class RegistrationView(View):
     def get(self, request):
-        return render(request,"registration.html")
+        return render(request, "registration.html")
+
     def post(self, request):
         print(request.POST.get("f_name"))
         print(request.POST.get("l_name"))
