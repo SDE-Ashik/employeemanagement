15dfaf3a3da5e1fde4c5a4c9ba54ed9af90fef05
ashik <ashiashik5047@gmail.com>
2022-06-22 22:34:17 +0530

    modified

--- employee/forms.py
+++ employee/forms.py
@@ -1,10 +1,13 @@
 from django import forms
+
+
 class RegistrationForm(forms.Form):
-    f_name = forms.CharField(label="Enter first name")
-    l_name = forms.CharField(label="Enter last name")
-    e_mail = forms.EmailField(label="Enter your email")
-    u_name = forms.CharField(label="Enter your Username")
-    pwd = forms.CharField(label="Enter your password")
+    f_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"FIRST NAME"}))
+    l_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "LAST NAME"}))
+    e_mail = forms.EmailField(widget=forms.EmailInput(attrs={"class":"form-control", "placeholder":"EMAIL"}))
+    u_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "USER NAME"}))
+    pwd = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder":"PASSWORD"}))
+
 
 class LoginForm(forms.Form):
     u_name = forms.CharField(label="enter your user name")
@@ -12,16 +15,20 @@ class LoginForm(forms.Form):
 
 
 class EmployeeForm(forms.Form):
-    eid = forms.CharField()
-    employee_name = forms.CharField()
-    designation = forms.CharField()
-    salary = forms.IntegerField()
-    email = forms.EmailField()
-    experience = forms.IntegerField()
+    eid = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control" , "placeholder":"enter eid"}))
+    employee_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    designation = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    salary = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    experience = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control"}))
 
     def clean(self):
         cleaned_data = super().clean()
         exp = cleaned_data.get("experience")
+        sal = cleaned_data.get("salary")
         if exp < 0:
             msg = "INVALID EXPERIENCE"
             self.add_error("experience", msg)
+        if  sal< 0:
+            msg = "SALARY ALWAYS POSTIVE "
+            self.add_error("salary", msg)
--- mathcalculation/views.py
+++ mathcalculation/views.py
@@ -8,15 +8,15 @@ from mathcalculation.forms import MathForm
 class AddView(View):
     def get(self, request):
         form = MathForm()
-        return render(request, "add.html", {"form": form})
+        return render(request, "add1.html", {"form": form})
     def post(self, request):
         form = MathForm(request.POST)
         if not form.is_valid():
-            return render(request,"add.html", {"form": form})
+            return render(request,"add1.html", {"form": form})
         n1 = form.cleaned_data.get("num1")
         n2 = form.cleaned_data.get("num2")
         result = n1 + n2
-        return render(request,"add.html",{"addres":result})
+        return render(request,"add1.html",{"addres":result})
 
 
 
@@ -36,15 +36,15 @@ class SubView(View):
 class MulView(View):
     def get(self, request):
         formm = MathForm()
-        return render(request,"mul.html",{"form":formm})
+        return render(request,"mul1.html",{"form":formm})
     def post(self,request):
         formm = MathForm(request.POST)
         if not formm.is_valid():
-            return render(request,"mul.html", {"form":formm})
+            return render(request,"mul1.html", {"form":formm})
         n1 = formm.cleaned_data.get("num1")
         n2 = formm.cleaned_data.get("num2")
         result = n1* n2
-        return render(request,"mul.html", {"mures":result})
+        return render(request,"mul1.html", {"mures":result})
 
 class DiviForm(View):
     def get(self,request):
@@ -57,4 +57,4 @@ class DiviForm(View):
         n1 = formm.cleaned_data.get("num1")
         n2 = formm.cleaned_data.get("num2")
         result = n1 / n2
-        return render(request,"div1.html",{"div":result})
+        return render(request,"div1.html",{"div":result, "form": formm})
